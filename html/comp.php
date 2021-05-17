ee6173e265255b922999cedfac38252c2cb1874c
syungo0926 <82852686+syungo0926@users.noreply.github.com>
2021-05-17 14:40:42 +0900

    Create comp.php

---
+++ html/comp.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>memo-app</title>
+  </head>
+  <body>
+    <h1>メモアプリ</h1>
+    <h2>登録完了</h2>
+  </body>
+</html>
